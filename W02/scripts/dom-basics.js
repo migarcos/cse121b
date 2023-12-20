ff129a8976ecd4fdfed36969c439826dbabb0fc6
migarcos <migark@hotmail.com>
2023-12-20 14:09:53 -0500

    upd w02

--- W02/scripts/dom-basics.js
+++ W02/scripts/dom-basics.js
@@ -1,3 +1,26 @@
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript";
 document.body.appendChild(newParagraph);
+
+const newImg = document.createElement("img");
+newImg.setAttribute('src', 'https://picsum.photos/200');
+newImg.setAttribute('alt', 'practicing how to apped a img with js');
+document.body.appendChild(newImg);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+// --- Insertion DIV ---
+const newSection = document.createElement("div");
+
+const newH2 = document.createElement("h2");
+newH2.innerText = "CS 121b";
+const newP = document.createElement("p");
+newP.innerText = "Welcome to Javascript  Language";
+
+newSection.appendChild(newH2);
+newSection.appendChild(newP);
+
+document.body.appendChild(newSection);
+
